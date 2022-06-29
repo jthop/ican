61189b2ed275ea0572c4e9aaa8731d675f06a755
jthop <2892650+jthop@users.noreply.github.com>
2022-06-29 18:26:45 -0500

    auto-commit triggered by version bump

--- ican/__init__.py
+++ ican/__init__.py
@@ -2,8 +2,8 @@
 """
 """
 
-__version__ = '0.1.5+build.100'
-__public_version__ = '0.1.5'
+__version__ = '0.1.6+build.101'
+__public_version__ = '0.1.6'
 __author__ = '@jthop'
 __copyright__ = f'Copyright 2022 {__author__}'
 
