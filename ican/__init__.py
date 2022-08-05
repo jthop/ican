b94637c026b2261a13468f3a933b2c2dac948d80
jthop <2892650+jthop@users.noreply.github.com>
2022-08-05 16:18:28 -0500

    auto-commit for v0.1.40

--- ican/__init__.py
+++ ican/__init__.py
@@ -2,8 +2,8 @@
 """
 """
 
-__version__ = "0.1.39+build.288"
-__public_version__ = "0.1.39"
+__version__ = "0.1.40+build.289"
+__public_version__ = "0.1.40"
 __author__ = "@jthop"
 __copyright__ = f"Copyright 2022 {__author__}"
 
